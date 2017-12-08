7cfa529411216cd9c2b028b3f81b3df1d43e4d2e
Deb Tjoa <debtjoa@gmail.com>
2017-12-07 22:47:47 -0800

    more changes to labels and layout

--- js/solicit.js
+++ js/solicit.js
@@ -9,13 +9,14 @@ $(document).ready(function() { /* TRANSITION BETWEEN CONTACTED AND NOT CONTACTED
         $(".solicited-container").fadeIn("800", "swing");
         $(".wishlist-container").fadeOut("800", "swing");
     });
-    $.getJSON('https://spreadsheets.google.com/feeds/list/1sNJXzsSzy4g9jM5V7ujahbBggiOedVjBDRuRVVTZgAE/2/public/full?alt=json', function(data) {
+    $.getJSON('https://spreadsheets.google.com/feeds/list/1sNJXzsSzy4g9jM5V7ujahbBggiOedVjBDRuRVVTZgAE/1/public/full?alt=json', function(data) {
         var count_solicited = 0;
         var count_wishlist = 0;
         $.each(data.feed.entry, function(i, v) {
             var status = v.gsx$status.$t;
             var bizname = v.gsx$profilename.$t;
-            var item = v.gsx$lastyrdonation.$t;
+            var item_thisyr = v.gsx$thisyrdonation.$t;
+            var item_lastyr = v.gsx$lastyrdonation.$t;
             var owner = v.gsx$owner.$t;
             var row_style = "";
             /* SET COLOR AND STYLE OF ROW BASED ON STATUS */
@@ -24,10 +25,15 @@ $(document).ready(function() { /* TRANSITION BETWEEN CONTACTED AND NOT CONTACTED
             } else if (status === "Received" || status === "Donating" || status === "Might donate") {
                 row_style = "positive";
             }
-            /* IF NO ITEM, SET CLASS TO HIDE THAT TD IN TABLET SIZES AND BELOW */
-            var hidingitem = "";
-            if (item === "" || item === "n/a") {
-                hidingitem = 'class="hidewhennarrow" ';
+            /* IF NO ITEM THIS YR, SET CLASS TO HIDE THAT TD IN TABLET SIZES AND BELOW */
+            var hidingitem_thisyr = "";
+            if (item_thisyr === "" || item_thisyr === "n/a") {
+                hidingitem_thisyr = 'class="hidewhennarrow" ';
+            }
+            /* IF NO ITEM LAST YR, SET CLASS TO HIDE THAT TD IN TABLET SIZES AND BELOW */
+            var hidingitem_lastyr = "";
+            if (item_lastyr === "" || item_lastyr === "n/a") {
+                hidingitem_lastyr = 'class="hidewhennarrow" ';
             }
             /* IF NO OWNER, SET CLASS TO HIDE THAT TD IN TABLET SIZES AND BELOW */
             var hidingowner = "";
@@ -38,12 +44,12 @@ $(document).ready(function() { /* TRANSITION BETWEEN CONTACTED AND NOT CONTACTED
             /* IF DONOR HAS NOT BEEN CONTACTED, WRITE OUT ROW THAT FOCUSES ON HOW TO CONTACT */
             if (status != "Not yet contacted") {
                 count_solicited++;
-                var rowcontentcontacted = '<tr class="' + row_style + '"><td class="text-center">' + count_solicited + '</td><td data-label="#' + count_solicited + '" class="flt-name-sol">' + bizname + '</td><td class="nowrap flt-status" data-label="Status">' + status + '</td><td ' + hidingitem + 'data-label="Item Donated">' + item + '</td></tr>';
+                var rowcontentcontacted = '<tr class="' + row_style + '"><td class="text-center">' + count_solicited + '</td><td data-label="#' + count_solicited + '" class="flt-name-sol">' + bizname + '</td><td class="nowrap flt-status" data-label="Status">' + status + '</td><td ' + hidingitem_thisyr + 'data-label="Item(s) Donated or Requested">' + item_thisyr + '</td></tr>';
 
                 $('#solicited tbody').append(rowcontentcontacted);
             } else if (status === "Not yet contacted") {
                 count_wishlist++;
-                var rowcontentnotcontacted = '<tr class="' + row_style + '"><td class="text-center">' + count_wishlist + '</td><td data-label="#' + count_wishlist + '" class="flt-name-wish">' + bizname + '</td><td ' + hidingitem + 'data-label="Item Donated Last Year">' + item + '</td><td ' + hidingowner + ' data-label="Owner" class="flt-owner">' + owner + '</td></tr>';
+                var rowcontentnotcontacted = '<tr class="' + row_style + '"><td class="text-center">' + count_wishlist + '</td><td data-label="#' + count_wishlist + '" class="flt-name-wish">' + bizname + '</td><td ' + hidingitem_lastyr + 'data-label="Item(s) Donated Last Year">' + item_lastyr + '</td><td ' + hidingowner + ' data-label="Owner" class="flt-owner">' + owner + '</td></tr>';
                 $('#wishlist tbody').append(rowcontentnotcontacted);
             }
         });
